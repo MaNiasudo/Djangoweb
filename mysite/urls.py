9688668fd570980b226d9668265b2e956c1284ec
TahaEfazati <wheremania@gmail.com>
2024-11-20 22:45:05 +0330

    added and fixed urls and interface

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('' , include("website.urls")),
+    path('blog/', include("blog.urls")),
     path('admin/', admin.site.urls),
 ]
 
