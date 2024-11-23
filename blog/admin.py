072f85408b3c1b58f698750ca975c51ce8507a0c
TahaEfazati <wheremania@gmail.com>
2024-11-23 15:49:10 +0330

    added url in interface for our posts and their display

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title','counted_views', 'status','published_date', 'created_date')
+    list_filter = ('status',)
+    search_fields=['title','content']
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class post(models.Model):
+class Post(models.Model):
     #image
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -14,4 +14,9 @@ class post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-created_date']
     
+    def __str__(self):
+        return f"{self.title} - {self.id}"
